cc9397e4dd5a6317c16980cdde423763f91b876f
Derek Gwekwerere <makoregwekwerere@gmail.com>
2020-07-06 16:08:38 +0200

    Updates

---
+++ precache-manifest.b0153fb2b85fdf688b9816f3e9009cf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26752772041e7b77f4f9c4a92c58f69",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "a62c0e3a05c04b84b500",
+    "url": "/My-Portfolio/static/css/main.ec35515a.chunk.css"
+  },
+  {
+    "revision": "7c8a9a01a99f4871e71f",
+    "url": "/My-Portfolio/static/js/2.bc68ee0b.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/My-Portfolio/static/js/2.bc68ee0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62c0e3a05c04b84b500",
+    "url": "/My-Portfolio/static/js/main.5db13aa9.chunk.js"
+  },
+  {
+    "revision": "71a7c0da8b552b22199f",
+    "url": "/My-Portfolio/static/js/runtime-main.625ddec2.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/My-Portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/My-Portfolio/static/media/fifth.90616a5d.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/My-Portfolio/static/media/first.54fd4234.jpg"
+  },
+  {
+    "revision": "f47a213b039ffc71d8e7915506972300",
+    "url": "/My-Portfolio/static/media/profile.f47a213b.JPG"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/My-Portfolio/static/media/second.730958a2.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/My-Portfolio/static/media/third.a8b012a0.jpg"
+  }
+]);
